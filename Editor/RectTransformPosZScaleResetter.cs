e7662912cd43a2b7ca207eefd60e1bb40d1a553c
baba_s <shotababa1206@gmail.com>
2020-08-21 15:03:00 +0900

    first commit

---
+++ Editor/RectTransformPosZScaleResetter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal static class RectTransformPosZScaleResetter
+	{
+		[MenuItem( "CONTEXT/RectTransform/Reset Pos Z, Scale" )]
+		private static void Hoge( MenuCommand command )
+		{
+			var rectTransform = ( RectTransform ) command.context;
+
+			Undo.RecordObject( rectTransform, "Reset Pos Z, Scale" );
+
+			var localPosition = rectTransform.localPosition;
+			localPosition.z             = 0;
+			rectTransform.localPosition = localPosition;
+
+			rectTransform.localScale = Vector3.one;
+		}
+	}
+}
